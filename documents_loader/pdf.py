09bf6c39a47881df0334481e5f488f7a0138cca4
tehseenjavaidbeigh <tehseenjavaidbeigh@gmail.com>
2026-03-23 18:41:32 +0530

    textsplitter added

--- documents_loader/pdf.py
+++ documents_loader/pdf.py
@@ -2,16 +2,36 @@ from langchain_community.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import TokenTextSplitter
+
 load_dotenv()
+
+splitter = TokenTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 10
+)
+
 data = PyPDFLoader("documents_loader/dataset.pdf")
 docs = data.load()
-model = ChatMistralAI(model = "mistral-small-2506")
-template =ChatPromptTemplate.from_messages(
-    [("system","You are an AI that summarises the text"),
-     ("human","{data}")])
+# model = ChatMistralAI(model = "mistral-small-2506")
+# template =ChatPromptTemplate.from_messages(
+#     [("system","You are an AI that summarises the text"),
+#      ("human","{data}")])
 
-prompt = template.format_messages(data = docs[0].page_content)
+# prompt = template.format_messages(data = docs[0].page_content) # if we just write docs.page_content then it will give us error as in case of pdf we have many pages and each page has its own meta data na page content. Therefore we have to accurately specify the page_content of which page we want to summarise
 
-result = model.invoke(prompt)
-print(result.content)
+# result = model.invoke(prompt)
+# print(result.content)
 #unlike text file pdf will not have length of 1
+
+
+
+chunks = splitter.split_documents(docs)
+print(len(chunks))
+for i in chunks:
+    print(i.page_content)
+    print()
+    print()
+    print()
+    print()
+    print()
--- documents_loader/test.py
+++ documents_loader/test.py
@@ -1,9 +1,25 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+splitter = CharacterTextSplitter(
+    separator = "",
+    chunk_size = 10,
+    chunk_overlap = 1
+)
 data = TextLoader("documents_loader/data.txt")
+
 docs = data.load()
 # print(docs)
-
+chunks = splitter.split_documents(docs)
 # print(docs[0])
 
-print(docs[0].page_content)
+# print(len(docs))
 
+print(len(chunks))
+
+for i in chunks:
+    print(i.page_content)
+    print()
+    print()
+    print()
+    print()
+    print()
